f7a5ad8853fdebb920810b3e9eaf7289df2af262
Pramodh003 <pramodhbr29@gmail.com>
2022-10-11 06:14:30 +0000

    added dockerfile

--- app.py
+++ app.py
@@ -22,4 +22,4 @@ def predict():
     	return render_template('result.html', prediction=my_prediction)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
